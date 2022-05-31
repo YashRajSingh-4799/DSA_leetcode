d03be3155dc2224711b446b4614e8143d2a4c8a1
YashRajSingh-4799 <64195098+YashRajSingh-4799@users.noreply.github.com>
2022-05-31 19:34:44 +0530

    Time: 54 ms (95.91%), Space: 18.8 MB (92.77%) - LeetHub

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+
+    
+    bool hasAllCodes(string s, int k) {
+           const int powTwoOfK = 1 << k;
+        
+        int need = powTwoOfK;
+        vector<bool> found(need, false);
+        
+        int curr = 0;
+        for (int i=0; i<s.size(); i++) {
+            curr <<= 1; // shift left
+            curr &= ~powTwoOfK; // remove kth bit
+            if (s[i] == '1') curr |= 1; // on first bit if s[i] == 1
+            
+            if (i >= k-1 && !found[curr]) {
+                found[curr] = true;
+                need--;
+                if (need == 0) {
+                    return true;
+                }
+            }
+        }
+    
+        return false;
+    }
+};
