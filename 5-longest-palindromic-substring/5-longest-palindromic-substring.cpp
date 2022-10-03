73638e1bab0b1cb8d626981c48155514f92ac9d5
YashRajSingh-4799 <64195098+YashRajSingh-4799@users.noreply.github.com>
2022-10-03 23:42:34 +0530

    Time: 22 ms (80.91%), Space: 6.7 MB (99.70%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,43 +1,31 @@
-class Solution
-{
-    public:
-        string longestPalindrome(string s)
-        {
-            int n = s.size();
-            int dp[n][n];
-
-            memset(dp, 0, sizeof(dp));
-            int end = 1;
-            int strt = 0;
-
-            for (int i = 0; i < n; i++)
-            {
-                dp[i][i] = 1;
-            }
-            for (int i = 0; i < n - 1; i++)
-            {
-                if (s[i] == s[i + 1])
-                {
-                    dp[i][i + 1] = 1;
-                    strt = i;
-                    end = 2;
+class Solution {
+public:
+    string longestPalindrome(string s) {
+         int ansR = -1, ansL = -1;
+        int mxLen = 0, strLen = s.length();
+        
+        for (int i = 0; i < strLen; i++){
+            int l = i, r = i;
+            while (l > -1 && r < strLen && s[l] == s[r]){
+                if (mxLen < (r-l+1)){
+                    ansR = r;
+                    ansL = l;
+                    mxLen = (r-l+1);
                 }
+                l--; r++;
             }
-            for (int j = 2; j < n; j++)
-            {
-                for (int i = 0; i < n - j; i++)
-                {
-                    int lft = i;
-                    int rght = i + j;
-
-                    if (dp[lft + 1][rght - 1] == 1 && s[lft] == s[rght])
-                    {
-                        dp[lft][rght] = 1;
-                        strt = i;
-                        end = j + 1;
-                    }
+            
+            l = i; r = i+1;
+            while(l > -1 && r < strLen && s[l] == s[r]){
+                if (mxLen < (r-l+1)){
+                    ansR = r;
+                    ansL = l;
+                    mxLen = (r-l+1);
                 }
+                l--; r++;
             }
-            return s.substr(strt, end);
         }
+        
+        return s.substr(ansL, (ansR - ansL + 1));
+    }
 };
