e39f42453165b0134fd39572f79c753204cd3f29
YashRajSingh-4799 <64195098+YashRajSingh-4799@users.noreply.github.com>
2022-11-09 21:57:36 +0530

    Time: 33 ms (42.46%), Space: 13.2 MB (55.39%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,77 +1,32 @@
-class Solution {
-public:
-     void setZeroes(vector<vector<int>>& matrix) {
-  
-        int m =matrix.size();   //row size
-        int n = matrix[0].size();   //column size
-		
-		 // for the first row & first col set x=1 & y=1 respectively as flag values.
-        int x =1;
-        int y =1;
-		
-		// marking x = 0 if any column element is 0
-        for(int j=0;j<n;j++)
+class Solution
+{
+    public:
+        void setZeroes(vector<vector < int>> &matrix)
         {
-            if(matrix[0][j]==0)
-			     x=0;  
-        }
-        
-		// marking y = 0 if any row element is 0
-        for(int i=0;i<m;i++)
-        {
-            if(matrix[i][0]==0)
-			     y=0;
-        }
-       
-         // Now checking if elements is 0 starting from row index 1 and column index 1 if yes then marking the zeroth index row and column element of respective index element as 0 
-        for(int i=1;i<m;i++)
-        {
-            for(int j=1;j<n;j++)
+            int col0 = 1;
+            int row = matrix.size();
+            int col = matrix[0].size();
+
+            for (int i = 0; i < row; i++)
             {
-                if(matrix[i][j]==0)
+                if (matrix[i][0] == 0)
+                    col0 = 0;
+                for (int j = 1; j < col; j++)
                 {
-                    matrix[i][0]=0;
-                    matrix[0][j]=0;
+                    if (matrix[i][j] == 0)
+                    {
+                        matrix[i][0] = matrix[0][j] = 0;
+                    }
                 }
             }
-        }
-        
-        // If any value of zeroth column is 0 then marking every element of that column as 0
-         for(int j=1;j<n;j++)
-        {
-           if(matrix[0][j]==0)
-           {
-               for(int i=1;i<m;i++)
-                   matrix[i][j]=0;
-           }
-        }
-        
-		  // If any value of zeroth row is 0 then marking every element of that row as 0
-        for(int i=1;i<m;i++)
-        {
-            if(matrix[i][0]==0)
-            {
-                for(int j=1;j<n;j++)
-                    matrix[i][j]=0;
+
+            for(int i=row-1; i>=0;i--){
+                for(int j=col-1; j>=1;j--){
+                    if(matrix[i][0]==0 || matrix[0][j]==0)
+                        matrix[i][j]=0;
+                }
+                if(col0==0)
+                    matrix[i][0]=0;
             }
         }
-        
-		//If y is 0 then mark all element in that row as 0
-        if(y==0)
-        {
-            for(int i=0;i<m;i++)
-            {
-                matrix[i][0]=0;
-            }
-        }
-        
-		//If x is 0 then mark all element in that column as 0
-         if(x==0)
-        {      
-             for(int j=0;j<n;j++)
-			 {
-                    matrix[0][j]=0;
-			 }
-        }
-}		
 };
